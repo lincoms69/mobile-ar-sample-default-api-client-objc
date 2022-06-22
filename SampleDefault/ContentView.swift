077ccb781ad35be6f44402800918f2475c7dbec4
Rinks <sukimastar@gmail.com>
2022-06-22 17:26:55 +0900

    all

---
+++ SampleDefault/ContentView.swift
@@ -0,0 +1,48 @@
+//
+//  ContentView.swift
+//  SampleDefault
+//
+//  Created by shinzator60 on 2022/06/22.
+//
+
+import SwiftUI
+import RealityKit
+
+struct ContentView : View {
+    var body: some View {
+        return ARViewContainer().edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct ARViewContainer: UIViewRepresentable {
+    
+    func makeUIView(context: Context) -> ARView {
+        
+        let arView = ARView(frame: .zero)
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+        
+        // オクルージョンを有効化
+        arView.environment.sceneUnderstanding.options.insert(.occlusion)
+        // メッシュ表示の有効化
+        arView.debugOptions.insert(.showSceneUnderstanding)
+        
+        return arView
+        
+    }
+    
+    func updateUIView(_ uiView: ARView, context: Context) {}
+    
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
