1330178e80dde110ae3494d5c9abab5a032d2843
Rinks <sukimastar@gmail.com>
2022-06-24 22:00:30 +0900

    Depth(distance b/w camera and plane) process and AR-Visualization

--- SampleDefault/ContentView.swift
+++ SampleDefault/ContentView.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 import RealityKit
+import ARKit
 
 struct ContentView : View {
     var body: some View {
@@ -16,15 +17,17 @@ struct ContentView : View {
 
 struct ARViewContainer: UIViewRepresentable {
     
+    // Display AR space
     func makeUIView(context: Context) -> ARView {
         
-        let arView = ARView(frame: .zero)
+        // RealityKit's Main view
+        // let arView = ARView(frame: .zero)
+        let arView = ARView(frame: .zero, cameraMode: .ar, automaticallyConfigureSession: true)
+        arView.addTapGesture()
         
-        // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
-        
-        // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
+        // デバッグ用設定
+        // 処理の統計情報と、検出した3D空間の特徴点を表示する。
+        arView.debugOptions = [.showStatistics, .showFeaturePoints]
         
         // オクルージョンを有効化
         arView.environment.sceneUnderstanding.options.insert(.occlusion)
@@ -35,6 +38,7 @@ struct ARViewContainer: UIViewRepresentable {
         
     }
     
+    // Update AR space
     func updateUIView(_ uiView: ARView, context: Context) {}
     
 }
@@ -46,3 +50,73 @@ struct ContentView_Previews : PreviewProvider {
     }
 }
 #endif
+
+extension ARView {
+
+    func addTapGesture() {
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap(recognizer:)))
+        self.addGestureRecognizer(tapGesture)
+    }
+
+    @objc func handleTap(recognizer: UITapGestureRecognizer) {
+
+        // タップしたロケーションを取得
+        let tapLocation = recognizer.location(in: self)
+
+        // タップした位置に対応する3D空間上の平面とのレイキャスト結果を取得
+        let raycastResults = raycast(from: tapLocation, allowing: .estimatedPlane, alignment: .any)
+
+        guard let firstResult = raycastResults.first else { return }
+        
+        // taplocationをワールド座標系に変換
+        let tapPosition = simd_make_float3(firstResult.worldTransform.columns.3)
+        print("TOUCH:  \(tapPosition)")
+        
+        let frame : ARFrame = session.currentFrame!
+        //let devicePosition = frame.camera.transform.columns.3
+        let devicePosition = simd_make_float3(frame.camera.transform.columns.3)
+        print("CAMERA: \(devicePosition)")
+        
+        let distance = distance(devicePosition, tapPosition)
+        print("DIST: \(distance)")
+        
+        placeCanvas(at: tapPosition, dist: distance)
+    }
+
+    /// キャンバスを配置する
+    private func placeCanvas(at position: SIMD3<Float>, dist distance: Float) {
+        // アンカーを作成
+        let anchor = AnchorEntity(world: position)
+
+        // テキストを作成
+        let textMesh = MeshResource.generateText(
+                    "d:\(distance * 100)cm",
+                    extrusionDepth: 0.1,
+                    font: .systemFont(ofSize: 1.0), // 小さいとフォントがつぶれてしまうのでこれぐらいに設定
+                    containerFrame: CGRect.zero,
+                    alignment: .left,
+                    lineBreakMode: .byTruncatingTail)
+
+        // 環境マッピングするマテリアルを設定
+        let textMaterial = SimpleMaterial(color: UIColor.yellow, roughness: 0.0, isMetallic: true)
+        let textModel = ModelEntity(mesh: textMesh, materials: [textMaterial])
+        textModel.scale = SIMD3<Float>(0.01, 0.01, 0.01) // 10分の1に縮小
+        textModel.position = SIMD3<Float>(0.0, 0.0, -0.02) // 奥0.2m
+        anchor.addChild(textModel)
+        
+        scene.anchors.append(anchor)
+
+    }
+
+    /// アートマテリアルを取得する
+    private func getArtMaterial(name resourceName: String) -> PhysicallyBasedMaterial? {
+
+        guard let texture = try? TextureResource.load(named: resourceName)
+        else { return nil }
+
+        var imageMaterial = PhysicallyBasedMaterial()
+        let baseColor = MaterialParameters.Texture(texture)
+        imageMaterial.baseColor = PhysicallyBasedMaterial.BaseColor(tint: .white, texture: baseColor)
+        return imageMaterial
+    }
+}
